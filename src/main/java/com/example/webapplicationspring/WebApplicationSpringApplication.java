88f4dab7fbf9cb01ad59b5ff14f22947ec03aae3
arek <arek1990cda@gmail.com>
2022-03-21 10:06:46 +0100

    init project

---
+++ src/main/java/com/example/webapplicationspring/WebApplicationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapplicationspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationSpringApplication.class, args);
+	}
+
+}
